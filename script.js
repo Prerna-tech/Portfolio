f958c40df5936dabe3c383b9b6077109fd2e0cde
SanidhyaPanwar <sanidhya.panwar1998@gmail.com>
2021-05-15 13:52:50 +0530

    troubleshooting

--- script.js
+++ script.js
@@ -1,51 +1,50 @@
-$(document).ready(function() {
-    $(window).ready(function(){
-       //Sticky navbar on Scroll script
-        if (this.scrollY > 20) {
-            $('.navbar').addClass("sticky");
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navibar on scroll script
+        if(this.scrollY > 20){
+            $('.navibar').addClass("sticky");
         }else{
-            $('.navbar').removeClass("sticky");
+            $('.navibar').removeClass("sticky");
         }
-
-        // Scroll up button show/hide Script
-        if (this.scrollY > 500) {
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
         }else{
             $('.scroll-up-btn').removeClass("show");
         }
     });
 
-    //Slide -up script
+    // slide-up script
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
-        //removing smooth scroll on slide-up button click
-        $('html').css("scrollBehaviour","auto");
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
     });
 
-    $('.navbar .menu li a').click(function(){
-        //applying again smooth scroll on menu items click
-        $('html').css("scrollBehaviour","smooth");
+    $('.navibar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
     });
 
-    //toggle text /navbar script
-    $('menu-btn').click(function(){
-        $('.navbar.menu').toggleClass("active");
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navibar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
     
-    //typing text animation script
-    var typed = new typed(".typing",{
-        String:["Learner","Innovator","Designer"],
-        typeSpeed:100,
-        backSpeed:60,
-        loop:true
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Learner", "Developer", "Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
     });
 
-    var typed =  new typed(".typing-2",{
-        strings: ["Learner","Innovator","Designer"],
-        typeSpeed:105,
-        backSpeed:60,
-        loop:true
+    var typed = new Typed(".typing-2", {
+        strings: ["Learner", "Developer", "Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
     });
 });
-
